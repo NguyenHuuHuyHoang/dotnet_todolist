7c8a695fc239b8a31849334466a3d1eee532598d
hoang <hoang.nhh1991@gmail.com>
2021-12-06 23:02:05 +0700

    update

--- dotnet_todolist/Controllers/WeatherForecastController.cs
+++ dotnet_todolist/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace dotnet_todolist.Controllers
 {
@@ -29,5 +30,9 @@ namespace dotnet_todolist.Controllers
             })
             .ToArray();
         }
+
+       
+
+        
     }
 }
---
+++ dotnet_todolist/Models/ToDo.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_todolist.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StatusId { get; set; }
+
+    }
+}
